fd115ac432180b76003066df04ad5afa395dd815
Akhror <akhsoliev@gmail.com>
2025-02-06 23:21:42 +0500

    feat: add Section component and update App layout with responsive styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
+import Section from "./components/Section";
 
 function App() {
   return (
     <>
       <Navbar />
+      <main>
+        <Section title="Analyze your text in real-time." />
+      </main>
     </>
   );
 }
---
+++ src/components/Section.jsx
@@ -0,0 +1,9 @@
+function Section({ title }) {
+  return (
+    <>
+      <h1 className="main-title">{title}</h1>
+    </>
+  );
+}
+
+export default Section;
