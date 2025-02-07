73e5d9de43528b4b327e35800649a8e936c5b51e
Akhror <akhsoliev@gmail.com>
2025-02-08 00:32:01 +0500

    feat: update Textarea component with new layout and options for user interaction

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -2,8 +2,19 @@ import "./Textarea.css";
 
 function Textarea() {
   return (
-    <div className="textarea">
-      <textarea className="container"></textarea>
+    <div className=" textarea-wrapper container">
+      <textarea className="textarea__container"></textarea>
+      <div className="options">
+        <label className="options__label">
+          <input className="options__input" type="checkbox" />
+          <span className="options__text">Exclude Spaces</span>
+        </label>
+        <label className="options__label">
+          <input className="options__input" type="checkbox" />
+          <span className="options__text">Set Character Limit</span>
+        </label>
+        <p className="approx-time">Approx. reading time: 1 minute</p>
+      </div>
     </div>
   );
 }
